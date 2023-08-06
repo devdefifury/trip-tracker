f0c1f98131c976da9a5800270dae6187992f9a3d
creativetemmy <temitopeayo01@gmail.com>
2023-08-06 23:55:15 +0100

    Added Router Functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 import Homepage from './pages/Homepage'
 import Product from './pages/Product'
@@ -6,16 +7,56 @@ import Pricing from './pages/Pricing'
 import AppLayout from './pages/AppLayout'
 import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
+import CityList from './components/CityList'
+import Form from './components/Form'
+import CountryList from './components/CountryList'
+import City from './components/City'
+
+const BASE_URL = 'http://localhost:8000'
 
 function App() {
+  const [cities, setCities] = useState([])
+  const [isLoading, setisLoading] = useState(false)
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setisLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCities(data)
+      } catch {
+        alert('There is an error laoding data...')
+      } finally {
+        setisLoading(false)
+      }
+    }
+    fetchCities()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,5 +1,21 @@
+import { NavLink } from 'react-router-dom'
+import styles from './AppNav.module.css'
+
 function AppNav() {
-  return <div>AppNav</div>
+  return (
+    <div className={styles.nav}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="cities">Cities</NavLink>
+          </li>
+          <li>
+            <NavLink to="countries">Countries</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default AppNav
